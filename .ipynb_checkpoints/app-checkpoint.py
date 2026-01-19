1dc2ca7c4c7698e2063a1e5bf398aeb825b6cc3a
Anbalagan Sairam <anbalagan.sairam@gmail.com>
2026-01-19 13:25:36 +0000

    Fixing lint issues

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -5,25 +5,26 @@ import json
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
-# Loading the model using joblib
+
 MODEL_PATH = "model.pkl"
 if not os.path.exists(MODEL_PATH):
     raise FileNotFoundError(f"{MODEL_PATH} not found")
 
-model = joblib.load(MODEL_PATH)  # Load model once at startup
+model = joblib.load(MODEL_PATH)
 
-# FastAPI App
+
 app = FastAPI(title="Titanic Survival Predictor")
 
-# Input Schema
+
 class PassengerFeatures(BaseModel):
     pclass: int
     age: float
     sibsp: int
     fare: float
 
-# Observability Middleware
+
 request_count = 0
+
 
 @app.middleware("http")
 async def log_requests(request: Request, call_next):
@@ -31,20 +32,25 @@ async def log_requests(request: Request, call_next):
     start_time = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
+
     request_count += 1
+
     log = {
         "request_number": request_count,
         "path": request.url.path,
         "method": request.method,
         "latency_sec": round(process_time, 4),
-        "status_code": response.status_code
+        "status_code": response.status_code,
     }
-    print(json.dumps(log))  # logs print the number of request made and time taken
+
+    print(json.dumps(log))
     return response
 
-# ----- Prediction Endpoint -----
+
 @app.post("/predict")
 def predict(features: PassengerFeatures):
     X = [[features.pclass, features.age, features.sibsp, features.fare]]
-    prob = model.predict_proba(X)[0][1]  # probability of survival (class 1)
+
+    prob = model.predict_proba(X)[0][1]
+
     return {"survival_probability": prob}
